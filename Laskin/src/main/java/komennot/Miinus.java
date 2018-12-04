dbd3ae30fe59a6bc06e9aac33da4f225531ffc3c
Tero Tapio <teromarkustapio@gmail.com>
2018-12-04 22:45:59 +0200

    refactors Miinus and Nolla to use inheritance

--- Laskin/src/main/java/komennot/Miinus.java
+++ Laskin/src/main/java/komennot/Miinus.java
@@ -4,48 +4,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import laskin.Sovelluslogiikka;
 
-public class Miinus implements Komento {
-    private final Sovelluslogiikka sovellus;
-    private final TextField syotekentta;
-    private final TextField tuloskentta;
-    private final Button nollaa;
-    private final Button undo;
+public class Miinus extends YhdenParametrinLaskutoimitus {
 
-    private String previousResult;
-
-    
     public Miinus(Sovelluslogiikka sovellus, TextField syotekentta, TextField tuloskentta, Button nollaa, Button undo) {
-        this.sovellus = sovellus;
-        this.syotekentta = syotekentta;
-        this.tuloskentta = tuloskentta;
-        this.nollaa = nollaa;
-        this.undo = undo;
+        super(sovellus, syotekentta, tuloskentta, nollaa, undo);
     }
-    
+
     @Override
     public void suorita() {
-        previousResult = tuloskentta.getText();
+        super.tallennaTulos();
         sovellus.miinus(Integer.parseInt(syotekentta.getText()));
-        undo.disableProperty().set(false);
-        syotekentta.setText("");
-        tuloskentta.setText("" + sovellus.tulos());
-        setNollaState();
-    }
-
-    @Override
-    public void peru() {
-        sovellus.setTulos(Integer.parseInt(previousResult));
-        tuloskentta.setText("" + previousResult);
-        syotekentta.setText("");
-    }
-
-        private void setNollaState() {
-        if (sovellus.tulos() == 0) {
-            nollaa.disableProperty().set(true);
-        } else {
-            nollaa.disableProperty().set(false);
-        }
-
+        super.yleinenSuoritus();
     }
 
 }
--- Laskin/src/main/java/komennot/Nollaa.java
+++ Laskin/src/main/java/komennot/Nollaa.java
@@ -4,48 +4,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import laskin.Sovelluslogiikka;
 
-public class Nollaa implements Komento {
-
-    private final Sovelluslogiikka sovellus;
-    private final TextField syotekentta;
-    private final TextField tuloskentta;
-    private final Button nollaa;
-    private final Button undo;
-
-    private String previousResult;
+public class Nollaa extends YhdenParametrinLaskutoimitus {
 
     public Nollaa(Sovelluslogiikka sovellus, TextField syotekentta, TextField tuloskentta, Button nollaa, Button undo) {
-        this.sovellus = sovellus;
-        this.syotekentta = syotekentta;
-        this.tuloskentta = tuloskentta;
-        this.nollaa = nollaa;
-        this.undo = undo;
+        super(sovellus, syotekentta, tuloskentta, nollaa, undo);
     }
 
     @Override
     public void suorita() {
-        previousResult = tuloskentta.getText();
+        super.tallennaTulos();
         sovellus.nollaa();
-        undo.disableProperty().set(false);
-        syotekentta.setText("");
-        tuloskentta.setText("" + sovellus.tulos());
-        setNollaState();
-    }
-
-    @Override
-    public void peru() {
-        sovellus.setTulos(Integer.parseInt(previousResult));
-        tuloskentta.setText("" + previousResult);
-        syotekentta.setText("");
-    }
-
-    private void setNollaState() {
-        if (sovellus.tulos() == 0) {
-            nollaa.disableProperty().set(true);
-        } else {
-            nollaa.disableProperty().set(false);
-        }
-
+        super.yleinenSuoritus();
     }
 
 }
